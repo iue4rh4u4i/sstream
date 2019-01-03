1fb33cdf92180926c9a046e8a634ccedb067dbc7
iue4rh4u4i <46347072+iue4rh4u4i@users.noreply.github.com>
2019-01-03 14:54:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,30 +7,4 @@ client.on('ready', () => {
   console.log('╚[════════════]╝')
 });
 
-
-
-const developers = ["510444339176931328"]
-const adminprefix = "-";
-client.on('message', message => {//Toxic Codes
-    var argresult = message.content.split(` `).slice(1).join(' ');//Toxic Codes
-      if (!developers.includes(message.author.id)) return;
-     
-  if (message.content.startsWith(adminprefix + 'setg')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else  
-  if (message.content.startsWith(adminprefix + 'setw')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});//Toxic Codes
-      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
-  } else
-  if (message.content.startsWith(adminprefix + 'setl')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
-  } else
-  if (message.content.startsWith(adminprefix + 'sets')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/zero");
-      message.channel.send(`**✅**`)//Toxic Codes
-  }
-});//Toxic Codes
-
 client.login(process.env.BlueGamer);
