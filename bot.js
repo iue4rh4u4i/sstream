865660fe6a4945e0a12cbbece1e98e64199018d0
iue4rh4u4i <46347072+iue4rh4u4i@users.noreply.github.com>
2019-01-03 14:30:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,4 +68,4 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {//Toxic Codes
 }
 });//Toxic Codes
 
-client.login(NTEwNDQ0MzM5MTc2OTMxMzI4.Dw-C7w.4v676FTW1Jag-VPCODzzcBEidIw);
+client.login("NTEwNDQ0MzM5MTc2OTMxMzI4.Dw-C7w.4v676FTW1Jag-VPCODzzcBEidIw");
