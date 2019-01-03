a56bd413f1e4e0ecb313175dcd6470b5e08ee216
iue4rh4u4i <46347072+iue4rh4u4i@users.noreply.github.com>
2019-01-03 14:41:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,27 +4,7 @@ const prefix = "-";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
   console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
 });
 
 
@@ -38,9 +18,6 @@ client.on('message', message => {//Toxic Codes
   if (message.content.startsWith(adminprefix + 'setg')) {
     client.user.setGame(argresult);
       message.channel.send(`**✅   ${argresult}**`)
-  } else
-     if (message.content === (adminprefix + "leave")) {//Toxic Codes
-    message.guild.leave();   //Toxic Codes
   } else  
   if (message.content.startsWith(adminprefix + 'setw')) {
   client.user.setActivity(argresult, {type:'WATCHING'});//Toxic Codes
@@ -54,18 +31,6 @@ client.on('message', message => {//Toxic Codes
     client.user.setGame(argresult, "https://www.twitch.tv/zero");
       message.channel.send(`**✅**`)//Toxic Codes
   }
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-  if (message.content.startsWith(adminprefix + 'setprefix')) {//Toxic Codes
-  client.user.setPrefix(argresult).then
-      message.channel.send(`Changing Prefix ..**${argresult}** `)//Toxic Codes
-} else
-if (message.content.startsWith(adminprefix + 'setavatar')) {//Toxic Codes
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);//Toxic Codes
-}
 });//Toxic Codes
 
 client.login("NTEwNDQ0MzM5MTc2OTMxMzI4.Dw-C7w.4v676FTW1Jag-VPCODzzcBEidIw");
