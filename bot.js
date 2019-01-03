5938f454ce2393904f7b25b9d1f4ac725aaa29e7
johnmuheeb3 <44290389+johnmuheeb3@users.noreply.github.com>
2019-01-03 13:47:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,48 +68,4 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {//Toxic Codes
 }
 });//Toxic Codes
 
-
-
-
-const develop = ["349095859859881984"]
-const adminpre = "-";
-client.on('message', message => {//Toxic Codes
-    var argresult = message.content.split(` `).slice(1).join(' ');//Toxic Codes
-      if (!develop.includes(message.author.id)) return;
-     
-  if (message.content.startsWith(adminpre + 'setg')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else
-     if (message.content === (adminpre + "leave")) {//Toxic Codes
-    message.guild.leave();   //Toxic Codes
-  } else  
-  if (message.content.startsWith(adminpre + 'setw')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});//Toxic Codes
-      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
-  } else
-  if (message.content.startsWith(adminpre + 'setl')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
-  } else
-  if (message.content.startsWith(adminpre + 'sets')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/zero");
-      message.channel.send(`**✅**`)//Toxic Codes
-  }
-  if (message.content.startsWith(adminpre + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-  if (message.content.startsWith(adminpre + 'setprefix')) {//Toxic Codes
-  client.user.setPrefix(argresult).then
-      message.channel.send(`Changing Prefix ..**${argresult}** `)//Toxic Codes
-} else
-if (message.content.startsWith(adminpre + 'setavatar')) {//Toxic Codes
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);//Toxic Codes
-}
-});//Toxic Codes
-
-
-
 client.login(process.env.BlueGamer);
